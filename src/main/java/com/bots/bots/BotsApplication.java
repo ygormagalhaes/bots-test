df6851a46d8a12c8204937d22283879c13d8af21
Ygor Magalhães <emaildoygor@gmail.com>
2019-09-25 20:01:38 -0300

    Commit inicial

---
+++ src/main/java/com/bots/bots/BotsApplication.java
@@ -0,0 +1,13 @@
+package com.bots.bots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotsApplication.class, args);
+	}
+
+}
